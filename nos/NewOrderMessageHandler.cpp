c966a0870d66ab1daa33b6983de2c6af63b57f7b
James Reinke <james.reinke@tastytrade.com>
2020-03-04 09:10:46 -0600

    OE-1275 - Safeguarding against some exceptions that can be thrown (#82) (#83)
    * Re-add try/catch for potential exceptions in parsing JSON to be safe and not risk a crash in prod.
    
    * Removed un-needed variable
    
    * Adding some includes this file is using that are included through other headers

--- nos/NewOrderMessageHandler.cpp
+++ nos/NewOrderMessageHandler.cpp
@@ -1,6 +1,8 @@
 #include "NewOrderMessageHandler.h"
 
-#include <twLib/ExceptionTypes.h>
+#include <string>
+#include <stdexcept>
+
 #include <twLib/JsonOrderInterpreter.h>
 #include <twLib/mq/MQAdapter.h>
 #include <twLib/mq/MQUtil.h>
@@ -9,43 +11,52 @@
 
 
 NewOrderMessageHandler::NewOrderMessageHandler(TW::OR2Adapter *pOR2Adapter, TW::MQAdapter *pMQAdapter, ORConfigReader::Config &config, TW::SenderLocationReader *pSenderLocationReader)
-  : m_pOR2Adapter(pOR2Adapter), m_pMQAdapter(pMQAdapter), m_pSenderLocationReader(pSenderLocationReader), m_config(config)
+  : m_pOR2Adapter(pOR2Adapter)
+  , m_pMQAdapter(pMQAdapter)
+  , m_pSenderLocationReader(pSenderLocationReader)
+  , m_config(config)
 {
   m_rootMap.initFromDBOption("db_option_combined.out", true);
 }
 
-bool NewOrderMessageHandler::handleMessage(nlohmann::json &jMessage, std::string UNUSED(strTopic)) {
+bool NewOrderMessageHandler::handleMessage(nlohmann::json &jMessage, std::string UNUSED(strTopic))
+{
   uint32_t nOrderNum = numeric_limits<uint32_t>::max();
-
-  std::string strRejectionMessage;
 
   SX_DEBUG("Attempting to handle message for order %s\n", jMessage.dump());
 
   TW::JsonOrderInterpreter orderWrapper = TW::JsonOrderInterpreter(jMessage, m_pSenderLocationReader);
   const string strDestination = MQUtil::extractDestination(jMessage, m_pOR2Adapter->getDefaultRoute());
 
-  sxORMsgWithType szMsg = orderWrapper.to_OR2MessageStruct(m_rootMap);
+  try {
+    sxORMsgWithType szMsg = orderWrapper.to_OR2MessageStruct(m_rootMap);
 
-  if (szMsg.h.uchType == sxORMsgWithType::MSG_INVALID) {
-    SX_DEBUG("Failed to create order message\n");
-    rejectMessageOrder(orderWrapper, "Failed to create order message");
+    if (szMsg.h.uchType == sxORMsgWithType::MSG_INVALID) {
+      SX_ERROR("Failed to create order message\n");
+      rejectMessageOrder(orderWrapper, "Failed to create order message");
+      return false;
+    }
+
+    // The OEL doesn't know about this account, reject this order
+    if (m_config.hmAccountMap.find(orderWrapper.getAccountNumber()) == m_config.hmAccountMap.end()) {
+      rejectMessageOrder(orderWrapper, "This account is unknown, it may not be configured for this product");
+      return false;
+    }
+
+    if (szMsg.h.uchType == sxORMsgWithType::MSG_NEW_ORDER_WITH_ACCOUNT) {
+      nOrderNum = m_pOR2Adapter->sendOrder(szMsg.u.nowa, strDestination);
+      SX_DEBUG("Sent order (single): %d \n", szMsg.u.nowa.nIdentifier);
+    }
+    else if (szMsg.h.uchType == sxORMsgWithType::MSG_COMPLEX_WRAPPER) { // Order is a spread (multi-leg)
+      msg_ComplexOrderWrapper &comp = szMsg.u.comp;
+      msg_NewOrderWithAccount *pNowa = (msg_NewOrderWithAccount *)comp.beginMsgStruct();
+      nOrderNum = m_pOR2Adapter->sendOrder(*pNowa, comp.getLeg(0), comp.nLegs, strDestination);
+      SX_DEBUG("Sent order (complex): %d \n", pNowa->nIdentifier);
+    }
+  } catch (const std::exception &e) {
+    SX_ERROR("Failed to process order - JSON Parser Error %s \n", e.what());
+    m_pMQAdapter->publishToErrorQueue(std::string("Failed to process order - JSON Parser Error ") + e.what());
     return false;
-  }
-
-  // The OEL doesn't know about this account, reject this order
-  if (m_config.hmAccountMap.find(orderWrapper.getAccountNumber()) == m_config.hmAccountMap.end()) {
-    rejectMessageOrder(orderWrapper, "This account is unknown, it may not be configured for this product");
-    return false;
-  }
-
-  if (szMsg.h.uchType == sxORMsgWithType::MSG_NEW_ORDER_WITH_ACCOUNT) {
-    nOrderNum = m_pOR2Adapter->sendOrder(szMsg.u.nowa, strDestination);
-    SX_DEBUG("Sent order (single): %d \n", szMsg.u.nowa.nIdentifier);
-  } else if (szMsg.h.uchType == sxORMsgWithType::MSG_COMPLEX_WRAPPER) { // Order is a spread (multi-leg)
-    msg_ComplexOrderWrapper &comp = szMsg.u.comp;
-    msg_NewOrderWithAccount *pNowa = (msg_NewOrderWithAccount *) comp.beginMsgStruct();
-    nOrderNum = m_pOR2Adapter->sendOrder(*pNowa, comp.getLeg(0), comp.nLegs, strDestination);
-    SX_DEBUG("Sent order (complex): %d \n", pNowa->nIdentifier);
   }
 
   // We get the bogus client order number in one of two circumstances:
@@ -54,13 +65,11 @@ bool NewOrderMessageHandler::handleMessage(nlohmann::json &jMessage, std::string
   // In either case, the failure is an infrastructure failure, and not a semantic failure.
   bool isOrderBogus = (nOrderNum == numeric_limits<uint32_t>::max());
   if (isOrderBogus) {
-    const std::string strRejectionMessage =  "OR:" + strDestination + ": Order sequence number has reached its limit or client lost connection.";
-    rejectMessageOrder(orderWrapper,  strRejectionMessage);
+    const std::string strRejectionMessage = "OR:" + strDestination + ": Order sequence number has reached its limit or client lost connection.";
+    rejectMessageOrder(orderWrapper, strRejectionMessage);
   }
 
-
   return !isOrderBogus;
-
 }
 
 void NewOrderMessageHandler::rejectMessageOrder(const TW::JsonOrderInterpreter &orderWrapper, const std::string &strRejectionMessage) const {
